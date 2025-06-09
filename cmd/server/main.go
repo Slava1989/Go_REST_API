5f0760aa7962dd0106c1f247cb98044d9375040b
Slava Chirita <slavachirita@Slavas-MacBook-Pro-2.local>
2025-06-09 21:25:45 +0300

    Adds comment data service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/Slava1989/Go_REST_API/internal/comment"
+	"github.com/Slava1989/Go_REST_API/internal/db"
+	// "go/doc/comment"
+)
 
 // MARK: responsible for instantiation and startup of app
 func Run() error {
 	fmt.Println("startin up our app")
+
+	db, err := db.NewDataBase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate database")
+		return err
+	}
+
+	cmtService := comment.NewService(db)
+	fmt.Println(cmtService.GetComment(context.Background(),
+		"a32cb3c6-0a24-4484-8b6a-2a0e2beae999",
+	))
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/Slava1989/Go_REST_API
 
 go 1.24.1
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.18.3 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+)
